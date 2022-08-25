edcfeff6a7b38b2961ddbd03226849b15c9ce87c
Bryan <suprnewb@gmail.com>
2022-08-25 13:58:34 +1000

    Initial setup

---
+++ app.js
@@ -0,0 +1,31 @@
+function ship(length, hit, isSunk) {
+  return {
+    length,
+    hit(n) {
+      //If n = ship position
+      //Mark as hit
+    },
+    isSunk() {
+      //use length to determine positions hit
+    }
+  }
+}
+
+function gameboard() {
+
+  function receiveAttack() {
+    //Take 2 coordinates
+    //Hit or miss?
+    //send result
+  }
+
+
+  //Display all shots
+
+  //Display winner results
+}
+
+
+function player() {
+
+}
